151a802d3024c859e53dbb1eef0fc3f3e6c7a1c1
Julia Orduño <juliapaola97@gmail.com>
2018-04-18 19:05:51 -0500

    new user ui

--- front/dist/js/user.controller.js
+++ front/dist/js/user.controller.js
@@ -14,25 +14,15 @@
 
     vm.style = function () {
       return {
-        "color": "#" + vm.user.profile_link_color
+        "color": "#" + vm.user.profile_link_color,
       }
     }
 
-    var data = {
-      // A labels array that can contain any sort of values
-      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-      // Our series array that contains series objects or in this case series data arrays
-      series: [
-        [5, 2, 4, 2, 0]
-      ]
-    };
-
-    // Create a new line chart object where as first parameter we pass in a selector
-    // that is resolving to our chart container element. The Second parameter
-    // is the actual data object.
-    new Chartist.Line('.ct-chart', data);
-
-    //////////////////
+    vm.background = function () {
+      return {
+        "background-color": "#" + vm.user.profile_link_color,
+      }
+    }
 
     vm.user = {
       "contributors_enabled": false,
@@ -134,6 +124,15 @@
       "verified": false
     }
 
+    function mostFrequentWords() {
+      var data = {
+        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+        series: [
+          [5, 2, 4, 2, 0]
+        ]
+      };
+      new Chartist.Bar('.ct-chart-words', data);
+    }
 
     function activate() {
       api
@@ -162,9 +161,16 @@
         console.log(error);
       }
 
+      mostFrequentWords();
 
       ///
 
     }
   }
 })();
+
+/**
+ * Variables
+ * sentiment analysis (muy positivas y/o negativas) 1 positiva, 0 negativa
+ * tiempo de respuesta
+ */
